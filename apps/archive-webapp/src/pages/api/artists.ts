361abc24b27a40edbb7443f8d1e8448625273ab7
hyejin-1010 <hyepago@gmail.com>
2025-02-25 21:05:59 +0900

    feat. Artist list get - group_id : Artist join

--- apps/archive-webapp/src/pages/api/artists.ts
+++ apps/archive-webapp/src/pages/api/artists.ts
@@ -1,16 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { Tables } from '../../../../../types_db.ts';
+import type { Tables } from "~/types_db.ts";
+import { createClient } from "~/utils/supabase/server.tsx";
 
 export default async function GET(
   _: NextApiRequest,
-  res: NextApiResponse<{ Artist: Tables<'Artist'> }>,
+  res: NextApiResponse<{ Artist: Tables<'Artist'>[] | null }>,
 ) {
-  const { createClient } = require('../../../../../utils/supabase/server.tsx');
   const supabase = await createClient();
     
-  const { data: Artist } = await supabase
-  .from('Artist')
-  .select();
+  const { data: Artist } = await supabase.from('Artist').select(
+    `
+      *, 
+      group:Artist!group_id(
+        *
+      )
+    `
+  );
 
   res.status(200).json({ Artist });
 }
