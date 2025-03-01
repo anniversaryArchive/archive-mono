bef4846eb60b3efcdbbe698455472723e0c56c49
chochozabe <durudduwls@gmail.com>
2025-03-02 00:17:32 +0900

    feat: antd 세팅

--- apps/archive-webapp/src/pages/_app.tsx
+++ apps/archive-webapp/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+
+import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 
+import globalToken from "@repo/antd-config/globalToken";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider {...globalToken}>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
