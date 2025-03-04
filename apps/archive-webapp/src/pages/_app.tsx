2ed318d9a4c1bc925508f2e25c84c0bca945b7ac
chochozabe <durudduwls@gmail.com>
2025-03-04 13:16:36 +0900

    modify: package → utils 전역 파일로 변경

--- apps/archive-webapp/src/pages/_app.tsx
+++ apps/archive-webapp/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 
-import globalToken from "@repo/antd-config/globalToken";
+import globalToken from "../../../../utils/antd/globalToken";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
