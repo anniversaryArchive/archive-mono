26b2928a8c3ad937ef3e9a5b8dcb99d0633aa115
Kim Yeo Kyoung <39088247+chochozabe@users.noreply.github.com>
2025-03-05 16:05:27 +0900

    🛠  antd 세팅 (#2)
    * feat: antd 세팅
    
    * refactor: 불필요한 속성 삭제
    
    * feat: 폰트 설정
    
    * feat: 웹앱 레이아웃
    
    * style: 반응형 width 적용
    
    * modify: 하단네이게이션바 수정
    
    * modify: package → utils 전역 파일로 변경
    
    * feat: 색상코드 전역변수로 설정
    
    * chore: 사용하지 않는 패키지 삭제
    
    * chore: 사용하지 않는 패키지 삭제
    
    ---------
    
    Co-authored-by: lia <68046496+kimsojung318@users.noreply.github.com>

---
+++ apps/archive-webapp/src/components/layout/Footer.tsx
@@ -0,0 +1,95 @@
+import {
+  EnvironmentOutlined,
+  HeartOutlined,
+  HomeOutlined,
+  RobotOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+import { Menu as AntdMenu } from "antd";
+import Link from "next/link";
+import styled from "styled-components";
+
+// TODO: #1 PR 병합 후 ~/context/global로 변경
+import { PRIMARY_COLOR } from "../../../../../contexts/global";
+
+export default function Footer() {
+  const menuList = [
+    {
+      label: (
+        <MenuLabel href={"/map"}>
+          <EnvironmentOutlined />
+          <span>지도</span>
+        </MenuLabel>
+      ),
+      key: "map",
+    },
+    {
+      label: (
+        <MenuLabel href={"/wish-list"}>
+          <HeartOutlined />
+          <span>마음함</span>
+        </MenuLabel>
+      ),
+      key: "wish-list",
+    },
+    {
+      label: (
+        <MenuLabel href={"/"}>
+          <HomeOutlined />
+          <span>홈</span>
+        </MenuLabel>
+      ),
+      key: "home",
+    },
+    {
+      label: (
+        <MenuLabel href={"/search"}>
+          <SearchOutlined />
+          <span>검색</span>
+        </MenuLabel>
+      ),
+      key: "search",
+    },
+    {
+      label: (
+        <MenuLabel href={"/support"}>
+          <RobotOutlined />
+          <span>소통창구</span>
+        </MenuLabel>
+      ),
+      key: "support",
+    },
+  ];
+
+  return <Menu mode="horizontal" items={menuList} />;
+}
+
+const Menu = styled(AntdMenu)`
+  height: 65px;
+  & > li {
+    padding-top: 10px;
+    width: calc(100% / 5);
+
+    &:after {
+      border: 0 !important;
+    }
+
+    &:hover {
+      color: ${PRIMARY_COLOR}bb !important;
+    }
+  }
+`;
+
+const MenuLabel = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  cursor: pointer;
+
+  & > span:last-child {
+    user-select: none;
+    margin: 0 !important;
+    line-height: 1;
+  }
+`;
